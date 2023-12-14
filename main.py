6d008f9cb130f9dfce1723ea73f1496f5ed8f383
Rvaibhavv <rvaibhav615@gmail.com>
2023-12-14 18:46:52 +0530

    data transformation completion

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from src.mlProject import logger
 from mlProject.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from mlProject.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from mlProject.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 
 logger.info("welcome to our custom login")
@@ -27,4 +28,15 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+STAGE_NAME="data transformation stage"
+try:
+        logger.info(f">>>>stage {STAGE_NAME} started <<<<")
+        data_transformation= DataTransformationTrainingPipeline()
+        data_transformation.main()
+        logger.info(f">>>>> stage {STAGE_NAME} completed \n")
+except Exception as e:
+        logger.exception(e)
+        raise e     
 
